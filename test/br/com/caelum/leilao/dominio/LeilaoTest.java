b6d1c90da1dab8bf6ad768a1c139df9ff8349409
Ederson da Silva <ederson.silva@atlanticsolutions.com.br>
2016-12-28 13:36:27 -0200

    git ignore e test

---
+++ test/br/com/caelum/leilao/dominio/LeilaoTest.java
@@ -0,0 +1,20 @@
+package br.com.caelum.leilao.dominio;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LeilaoTest {
+	
+	@Test
+	public void deveDobrarOUtilmoLanceDado(){
+		Leilao leilao = new Leilao("Macbook pro 15");
+		Usuario steveJob = new Usuario("Steve Jobs");
+		Usuario billGates = new Usuario("Steve Jobs");
+		
+		leilao.propoe(new Lance(steveJob, 2000.00));
+		leilao.propoe(new Lance(billGates, 3000.00));
+		leilao.dobraLance(steveJob);
+		assertEquals(4000, leilao.getLances().get(2).getValor(),0.0001);
+	}
+}
